fb22241c662db9dc04b7c412ffd52783f880bdb6
Scott47 <scottsilver47@gmail.com>
2021-05-08 15:48:23 -0500

    models and some views setup to handle requests from level-up-client

---
+++ levelupapi/models/event.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Event(models.Model):
+    game        = models.ForeignKey("Game",
+                                    on_delete=models.CASCADE)
+    organizer   = models.ForeignKey("Gamer", 
+                                    on_delete=models.CASCADE)
+    start_date  = models.DateTimeField()
+    description = models.TextField()
+    attendees   = models.ManyToManyField("Gamer",
+                                        through="GamerEvent",
+                                        related_name="attending")
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
+
+        
---
+++ levelupapi/models/game.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Game(models.Model):
+
+    game_type            = models.ForeignKey("GameType",
+                                            on_delete=models.CASCADE)
+    maker               = models.CharField(max_length=255)
+    title               = models.CharField(max_length=50)
+    description         = models.CharField(max_length=255)
+    number_of_players   = models.IntegerField()
+    skill_level         = models.IntegerField()
+    gamer               = models.ForeignKey("Gamer",
+                                            on_delete=models.CASCADE)
