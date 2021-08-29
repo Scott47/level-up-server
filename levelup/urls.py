137cde84b81aa9b3b3d0695baa53ba9f0015c594
Scott47 <scottsilver47@gmail.com>
2021-08-29 14:28:52 -0500

    feat: profile view retrieves a single gamer profile when auth token is provided with any request

--- levelup/urls.py
+++ levelup/urls.py
@@ -20,12 +20,15 @@ from levelupapi.views import (register_user,
                                 login_user,
                                 GameTypeView,
                                 GameView,
-                                EventView)
+                                EventView,
+                                ProfileView)
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
 router.register(r'events', EventView, 'event')
+router.register(r'profile', ProfileView, 'profile')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +36,5 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('levelupreports.urls')),
 ]
